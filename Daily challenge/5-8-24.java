6777adc10dad2672fac096128815ddba4adc345c
balajiselvam005 <135320781+balajiselvam005@users.noreply.github.com>
2024-10-16 18:19:31 +0530

    Add files via upload

---
+++ Daily challenge/5-8-24.java
@@ -0,0 +1,24 @@
+// 1508. Range Sum of Sorted Subarray Sums
+
+import java.util.*;
+
+class Solution {
+
+    public String kthDistinct(String[] a, int k) {
+        HashMap<String, Integer> map = new HashMap<>();
+        ArrayList<String> list = new ArrayList<>();
+        for (String s : a) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+        for (String s : a) {
+            if (map.get(s) == 1) {
+                list.add(s);
+            }
+        }
+        if (list.size() < k) {
+            return "";
+        }
+
+        return list.get(k - 1);
+    }
+}
