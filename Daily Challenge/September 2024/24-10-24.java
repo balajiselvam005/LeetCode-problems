e828e74ffcf8c650969aa5036882e98b0a8cd03e
balajiselvam005 <135320781+balajiselvam005@users.noreply.github.com>
2024-11-01 00:24:23 +0530

    Add files via upload

---
+++ Daily Challenge/September 2024/24-10-24.java
@@ -0,0 +1,17 @@
+// 951. Flip Equivalent Binary Trees
+
+import javax.swing.tree.TreeNode;
+
+class Solution {
+
+    public boolean flipEquiv(TreeNode node1, TreeNode node2) {
+      if (node1 == null && node2 == null)
+        return true;
+  
+      if (node1 == null || node2 == null || node1.val != node2.val)
+        return false;
+  
+      return (flipEquiv(node1.left, node2.left) && flipEquiv(node1.right, node2.right)) ||
+          (flipEquiv(node1.left, node2.right) && flipEquiv(node1.right, node2.left));
+    }
+  }
---
+++ Daily Challenge/September 2024/25-10-24.java
@@ -0,0 +1,23 @@
+// 1233. Remove Sub-Folders from the Filesystem
+
+import java.util.*;
+
+class Solution {
+
+    public List<String> removeSubfolders(String[] folder) {
+        Arrays.sort(folder);
+
+        List<String> ans = new ArrayList<>();
+        ans.add(folder[0]);
+
+        for (int i = 1; i < folder.length; i++) {
+            String lastFolder = ans.get(ans.size() - 1) + "/";
+
+            if (!folder[i].startsWith(lastFolder)) {
+                ans.add(folder[i]);
+            }
+        }
+
+        return ans;
+    }
+}
